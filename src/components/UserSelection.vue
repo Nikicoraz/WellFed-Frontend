b6b0d54e48a51921c3c8299cb0a7b9c45574c6e6
Nikicoraz <nicolacorato05@gmail.com>
2025-12-09 22:47:03 +0100

    Daisyui e ritocchi schermate

---
+++ src/components/UserSelection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+    const props = defineProps({
+        name: String,
+        image: String,
+        callback: Function
+    });
+
+    if(!props.callback) {
+        console.error("No callback set");
+    }
+</script>
+
+<template>
+    <div class="cursor-pointer text-center h-20 outline outline-black/10 shadow-lg rounded-2xl" @click="props.callback!">
+        {{ $t(props.name!) }}
+        <div class="hover-3d w-full">
+            <figure class="w-full">
+                <img :src="props.image" alt="" class="aspect-square w-full h-10">
+            </figure>
+
+            <!-- 8 empty divs needed for the 3D effect -->
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+
+        </div>
+    </div>
+</template>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,46 +1,45 @@
 <script setup lang="ts">
-import VueCookies from "vue-cookies";
-import { router } from "../extensions/router";
-import WellfedLogoVue from "../components/WellfedLogo.vue";
+    import VueCookies from "vue-cookies";
+    import { router } from "../extensions/router";
+    import WellfedLogoVue from "../components/WellfedLogo.vue";
+    import { ref } from "vue";
 
-const email: string = "";
-const password: string = "";
+    const email = ref("");
+    const password = ref("");
 
-function login(email: string, password: string){
-    fetch(import.meta.env.VITE_BACKEND_URL + "/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        })
-    }).then(async e => {
-        if(e.status == 401) {
+    function login(){
+        fetch(import.meta.env.VITE_BACKEND_URL + "/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "email": email.value,
+                "password": password.value
+            })
+        }).then(async e => {
+            if(e.status == 401) {
+                alert("Authentication failed");
+                return;
+            }
+            const data = await e.json();
+            (VueCookies as any).set("token", data.token, 86400);
+            router.push(e.headers.get("Location") ?? "/");
+        }).catch(e => {
+            console.log(e);
             alert("Authentication failed");
-            return;
-        }
-        const data = await e.json();
-        (VueCookies as any).set("token", data.token, 86400);
-        console.log(e.headers);
-        console.log("Moving to " + e.headers.get("Location"));
-        router.push(e.headers.get("Location") ?? "/");
-    }).catch(e => {
-        console.log(e);
-        alert("Authentication failed");
-    });
-}
+        });
+    }
 </script>
 <template>
-    <div class="h-screen flex">
+    <div class="h-screen flex" @keypress.enter.native="login()">
         <div class="m-auto flex flex-col gap-2 max-w-sm items-center rounded-xl bg-white p-12 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
             <WellfedLogoVue></WellfedLogoVue>
             <p>{{ $t("login.credenziali") }}</p>
             <div class="flex flex-col gap-2">
-                <input type="email" v-model="email" placeholder="Email" class="input">
-                <input type="password" v-model="password" placeholder="Password" class="input">
-                <input type="submit" @click="login(email, password)" :value="$t('login.accedi')" class="btn-1">
+                <input type="email" v-model="email" placeholder="Email" class="input-1">
+                <input type="password" v-model="password" placeholder="Password" class="input-1">
+                <input type="submit" @click="login" :value="$t('login.accedi')" class="btn-1">
                 <input type="submit" value="Accedi con google" class="btn-1">
                 <p class="text-center"> {{ $t("login.oppure") }} </p>
                 <input type="submit" @click="router.push('/register')" :value="$t('login.registrati')" class="p-4 border border-black bg-lime-900 text-white rounded-lg hover:bg-lime-950">
@@ -51,7 +50,7 @@ function login(email: string, password: string){
 
 <style scoped >
     @reference "tailwindcss";
-    .input {
+    .input-1 {
         @apply border border-black rounded-lg text-center
     }
 
