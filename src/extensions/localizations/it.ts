831e2b19ce50724f5464613f6bef0da72ae41133
Nikicoraz <nicolacorato05@gmail.com>
2026-01-04 16:51:27 +0100

    Validazione partita iva

--- src/extensions/localizations/it.ts
+++ src/extensions/localizations/it.ts
@@ -14,6 +14,7 @@ export default {
         processando: "L'account sta venendo processato",
         creato: "L'account è stato creato",
         formatoPassword: "Almeno 8 caratteri, una lettera minuscola, una lettera maiuscola, un numero e un carattere speciale",
+        formatoPIVA: "11 numeri con opzionale prefisso IT",
         reqPassword: "La password non rispetta i requisiti minimi di sicurezza, deve essere lunga almeno 8 caratteri e contenere: una lettera minuscola, una lettera maiuscola, un numero e un carattere speciale",
         reqEmail: "L'email inserita non è valida!",
     },
